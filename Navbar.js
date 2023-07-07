0528176813b7fd7404316d659e1f081d62433c9e
Balagreen <128374776+Balagreen@users.noreply.github.com>
2023-07-07 12:34:38 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+
+const Navbar = () => {
+
+    return(
+        
+     <nav className="navbar">
+      <h1 className="cafe">Food Cafe</h1>
+      <img className="mid" src="Food.jpeg" alt="111"></img>
+      <ul>
+      <h3 className="cont">Contact:</h3>
+      </ul>
+      <ul>
+      <h4 className="name">Food Cafe</h4>
+      <h4 className="name">234,GST Road</h4>
+      <h4 className="name">Chennai-600001</h4>
+      </ul>  
+     </nav>
+     
+     )
+    }
+
+    export default Navbar
